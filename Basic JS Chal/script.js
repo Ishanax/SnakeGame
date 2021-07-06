a3b2dffb2ffa7b3856ac100bf2149dfbe254881b
Ishanax <67413079+Ishanax@users.noreply.github.com>
2021-07-06 10:59:31 +0200

    search bar finished

--- Basic JS Chal/script.js
+++ Basic JS Chal/script.js
@@ -1,20 +1,20 @@
 
-document.getElementById('searchInput').addEventListener('keyup', function(event){
-    let searchQuery = event.target.value.toLowerCase();
-    let allNamesDOMCollection = document.getElementsByClassName('name');
-    console.log(allNamesDOMCollection[1].textContent)
+// document.getElementById('searchInput').addEventListener('keyup', function(event){
+//     let searchQuery = event.target.value.toLowerCase();
+//     let allNamesDOMCollection = document.getElementsByClassName('name');
+//     console.log(allNamesDOMCollection[1].textContent)
 
-    for ( let counter = 0; counter < allNamesDOMCollection.length; counter++){
-        const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
-        console.log(currentName);
+//     for ( let counter = 0; counter < allNamesDOMCollection.length; counter++){
+//         const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
+//         console.log(currentName);
 
-        if (currentName.includes(searchQuery)){
-            allNamesDOMCollection[counter].style.display = "block";
-        }else{
-            allNamesDOMCollection[counter].style.display = "none";
-        }
-    }   
-})
+//         if (currentName.includes(searchQuery)){
+//             allNamesDOMCollection[counter].style.display = "block";
+//         }else{
+//             allNamesDOMCollection[counter].style.display = "none";
+//         }
+//     }   
+// })
 
 
 
