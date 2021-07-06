a9764aabb4c4b97269c891e5276e8b8a2d1f826b
Ishanax <67413079+Ishanax@users.noreply.github.com>
2021-07-06 15:31:06 +0200

    slider finished

--- Basic JS Chal/script.js
+++ Basic JS Chal/script.js
@@ -1,20 +1,38 @@
+const slides = document.getElementsByClassName('carousel-item');
 
-// document.getElementById('searchInput').addEventListener('keyup', function(event){
-//     let searchQuery = event.target.value.toLowerCase();
-//     let allNamesDOMCollection = document.getElementsByClassName('name');
-//     console.log(allNamesDOMCollection[1].textContent)
+let slidePosition = 0;
+const totalSlides = slides.length;
 
-//     for ( let counter = 0; counter < allNamesDOMCollection.length; counter++){
-//         const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
-//         console.log(currentName);
+document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide)
 
-//         if (currentName.includes(searchQuery)){
-//             allNamesDOMCollection[counter].style.display = "block";
-//         }else{
-//             allNamesDOMCollection[counter].style.display = "none";
-//         }
-//     }   
-// })
+document.getElementById('carousel-button-previous').addEventListener('click', moveToPrevSlide)
 
+function hideAllSlides (){
+    for(let slide of slides){
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+}
+
+
+function moveToNextSlide(){
+    hideAllSlides();
+    if(slidePosition === totalSlides -1){
+        slidePosition = 0;
+    }else{
+        slidePosition++;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
+
+function moveToPrevSlide(){
+    hideAllSlides();
+    if(slidePosition === 0){
+        slidesPosition = totalSlides - 1;
+    }else{
+        slidePosition--;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
 
 
